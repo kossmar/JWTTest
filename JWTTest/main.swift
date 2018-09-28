ebe49b6c720fd78c08f6ea96aab34c8c9d629506
Mar Koss <mkoss@mars-fiefdom-control-center.local>
2018-09-28 10:22:15 -0700

    Initial Commit

---
+++ JWTTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  JWTTest
+//
+//  Created by Mar Koss on 2018-09-28.
+//  Copyright © 2018 Mar Koss. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
